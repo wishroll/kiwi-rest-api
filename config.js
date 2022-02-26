820b7dcc06a419c42e1d014649f9599cb2dbda4f
Great Okonkwo <great@wishroll.co>
2022-02-25 19:11:43 -0500

    Wrap dotenv into development if statement

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
-const dotenv = require('dotenv');
-dotenv.config();
-module.exports = {
-  port: process.env.PORT,
-  environment: process.env.NODE_ENV,
-  masterKey: process.env.MASTER_KEY        
-};
+if(process.env.NODE_ENV == "development") {
+  const dotenv = require('dotenv');
+  dotenv.config();
+  module.exports = {
+    port: process.env.PORT,
+    environment: process.env.NODE_ENV,
+    masterKey: process.env.MASTER_KEY        
+  };
+}
