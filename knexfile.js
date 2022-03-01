19abc5f22e0ae2fb9b208a6e07631e982d8101f5
Great Okonkwo <gboyokonkwo35@gmail.com>
2022-03-01 01:18:07 -0500

    Remove ssl option

--- knexfile.js
+++ knexfile.js
@@ -44,7 +44,6 @@ module.exports = {
     client: 'postgresql',
     connection: {
       connectionString: process.env.DATABASE_URL,
-      ssl: false
     },
     pool: {
       min: process.env.MIN_CONNECTIONS || 10,
