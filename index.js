ef3516b77817342740caf290282eb4368efba902
Great Okonkwo <great@wishroll.co>
2022-03-02 17:04:10 -0500

    Change number

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ fastify.post('/signup/verify', (req, res) => {
     const countryCode = req.body['country_code'];
     phoneNumber = phone(phoneNumber, {country: countryCode})["phoneNumber"];
 
-    if (phoneNumber === '16462471839' && token === '000000') {
+    if (phoneNumber === '+16462471839' && token === '000000') {
         const cacheKey = `verified-phone-number-${phoneNumber}`;
         redisClient.client.set(cacheKey, token).then((_) => console.log("Cache Set the token that verifies a users phone number"));                
         return res.status(200).send({success: true});
