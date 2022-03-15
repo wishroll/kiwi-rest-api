209e25f93a8281df615b3a49f42e0872e53830e7
Great Okonkwo <gboyokonkwo35@gmail.com>
2022-03-14 20:34:11 -0400

    Hardcode

--- knexfile.js
+++ knexfile.js
@@ -4,45 +4,48 @@
  */
 module.exports = {
 
-  // development: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     host: '127.0.0.1',
-  //     username: 'greatokonkwo',
-  //     password: 'greatokonkwo',
-  //     database: 'mutual-api-server-development'
-  //   },
-  //   pool: {
-  //     min: process.env.MIN_CONNECTIONS || 10,
-  //     max: process.env.MAX_CONNECTIONS || 100
-  //   },
-  //   migrations: {
-  //     directory: './db/migrations',
-  //   },
-  //   seeds: { directory: './seeds' },
-  //   debug: true,
-  //   useNullAsDefault: true
-  // },
+  development: {
+    client: 'postgresql',
+    connection: {
+      host: '127.0.0.1',
+      username: 'greatokonkwo',
+      password: 'greatokonkwo',
+      database: 'mutual-api-server-development'
+    },
+    pool: {
+      min: process.env.MIN_CONNECTIONS || 10,
+      max: process.env.MAX_CONNECTIONS || 100
+    },
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: { directory: './seeds' },
+    debug: true,
+    useNullAsDefault: true
+  },
 
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     directory: './migrations',
-  //   }
-  // },
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './migrations',
+    }
+  },
 
   production: {
     client: 'postgresql',
-    connection: "mutuals-database-2.cbnjgsccdjeo.us-east-2.rds.amazonaws.com",
+    connection: {
+      connectionString: "mutuals-database-2.cbnjgsccdjeo.us-east-2.rds.amazonaws.com",
+      ssl: { rejectUnauthorized: false }
+    },
     pool: {
       min: process.env.MIN_CONNECTIONS || 10,
       max: process.env.MAX_CONNECTIONS || 100
