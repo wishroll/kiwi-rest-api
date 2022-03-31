da5106872bc50a00288a3f670085215c4a7d425a
Great Okonkwo <great@wishroll.co>
2022-03-31 18:31:58 -0400

    Add fetch to app modules

--- services/api/spotify/spotify_authorization_controller.js
+++ services/api/spotify/spotify_authorization_controller.js
@@ -1,5 +1,6 @@
 const routes = async (fastify, options) => {
     const crypto = require('crypto')
+    const fetch = require("node-fetch");
     const spotifyAuthUri = process.env.SPOTIFY_AUTH_URI
     const state = crypto.randomBytes(16).toString('hex')
 
