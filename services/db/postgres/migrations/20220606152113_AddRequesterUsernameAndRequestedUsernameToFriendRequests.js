2bdcbafba6d6a61ea5f25f92f11cd0380aa42c4c
Great Okonkwo <great@wishroll.co>
2022-06-07 14:52:18 -0400

    Return a users sent songs

--- services/db/postgres/migrations/20220606152113_AddRequesterUsernameAndRequestedUsernameToFriendRequests.js
+++ services/db/postgres/migrations/20220606152113_AddRequesterUsernameAndRequestedUsernameToFriendRequests.js
@@ -6,13 +6,15 @@ exports.up = function(knex) {
     return knex.schema.alterTable('friend_requests', (table) => {
         table.setNullable('requester_phone_number')
         table.setNullable('requested_phone_number')
-        table.string('requester_username', 255)
-        table.string('requested_username', 255)
-        table.unique(['requester_username', 'requested_username'], {
-            indexName: 'index_requester_username_requested_username',
+        table.bigInteger('requester_user_id').notNullable()
+        table.bigInteger('requested_user_id').notNullable()
+        table.foreign('requester_user_id').references('users.id')
+        table.foreign('requested_user_id').references('users.id')
+        table.unique(['requester_user_id', 'requested_user_id'], {
+            indexName: 'index_requester_user_id_requested_user_id',
             deferrable: 'immediate'
         })
-        table.index(['requester_username', 'requested_username'])
+        table.index(['requester_user_id', 'requested_user_id'])
     })
 };
 
@@ -22,8 +24,8 @@ exports.up = function(knex) {
  */
 exports.down = function(knex) {
     return knex.schema.alterTable('friend_requests', (table) => {
-        table.dropColumn('requester_username')
-        table.dropColumn('requested_username')
+        table.dropColumn('requester_user_id')
+        table.dropColumn('requested_user_id')
         table.dropNullable('requester_phone_number')
         table.dropNullable('requested_phone_number')
 
