b7fc541fb8887acffb9bb2435773b75a5f1d807b
Great Okonkwo <great@wishroll.co>
2022-07-21 03:10:49 -0400

    Remove repeating routes

--- services/api/spotify/spotify_controller.js
+++ services/api/spotify/spotify_controller.js
@@ -1,59 +1,6 @@
 const routes = async (fastify, options) => {
   const fetch = require('node-fetch')
   const {sendNotificationOnReceivedSong} = require('../../notifications/notifications')
-  fastify.get('/spotify/me/playlists', { onRequest: [fastify.authenticate] }, async (req, res) => {
-    const currentUserId = req.user.id
-    const accessCode = req.headers['spotify-authorization']
-    console.log(accessCode)
-    if (!accessCode) {
-      return res.status(401).send({ error: true, message: 'Missing Spotify Auth token' })
-    }
-
-    const url = 'https://api.spotify.com/v1/me/playlists'
-    const header = { 'Content-Type': 'application/json', Authorization: `Bearer ${accessCode}` }
-    try {
-      const response = await fetch(url, {
-        method: 'Get',
-        headers: header
-      })
-      if (response.ok) {
-        const data = await response.json()
-        res.status(200).send(data)
-      } else {
-        console.log(response.status, response.statusText)
-        res.status(response.status).send({ error: true, message: response.statusText })
-      }
-    } catch (error) {
-      res.status(500).send({ error: true, message: error })
-    }
-  })
-
-    fastify.get('/spotify/me/playlists', { onRequest: [fastify.authenticate] }, async (req, res) => {
-        const currentUserId = req.user.id
-        const accessCode = req.headers['spotify-authorization']
-        console.log(accessCode)
-        if (!accessCode) {
-            return res.status(401).send({ error: true, message: 'Missing Spotify Auth token' })
-        }
-
-        const url = 'https://api.spotify.com/v1/me/playlists'
-        const header = { 'Content-Type': 'application/json', 'Authorization': `Bearer ${accessCode}` }
-        try {
-            const response = await fetch(url, {
-                method: 'Get',
-                headers: header
-            })
-            if (response.ok) {
-                const data = await response.json()
-                res.status(200).send(data)
-            } else {
-                console.log(response.status, response.statusText)
-                res.status(response.status).send({ error: true, message: response.statusText })
-            }
-        } catch (error) {
-            res.status(500).send({ error: true, message: error })
-        }
-    })
 
     fastify.get('/spotify/tracks/recieved', { onRequest: [fastify.authenticate] }, async (req, res) => {
         const currentUserId = req.user.id
