6a89bb9366da54bf2077d1f80ac1712eefb0c95d
Great Okonkwo <great@wishroll.co>
2022-08-04 03:03:03 -0400

    Remove select

--- routes/messages/index.js
+++ routes/messages/index.js
@@ -114,7 +114,7 @@ module.exports = async (fastify, options) => {
   fastify.post('/v1/update/tracks', async (req, res) => {
     const limit = req.query.limit;
     const offset = req.query.offset;
-    fastify.knex('spotify_tracks').select(['id']).limit(limit).offset(offset).then((spotify_tracks) => {
+    fastify.knex('spotify_tracks').select().limit(limit).offset(offset).then((spotify_tracks) => {
       promiseAllInBatches((spotify_track) => {
         insertIntoTracksTable(spotify_track)
       }, spotify_tracks, limit)
