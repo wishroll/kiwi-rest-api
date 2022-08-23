0075af2cec0d5018a56fad4d57cf7a6cfa8e62c3
Great Okonkwo <great@wishroll.co>
2022-08-22 20:23:36 -0400

    Remove search temp

--- routes/search/index.js
+++ routes/search/index.js
@@ -10,33 +10,34 @@ module.exports = async (fastify, options) => {
       return res.status(400).send({ message: 'Missing query' })
     }
     try {
-      const users = await fastify.readDb('users').whereILike('username', `%${query}%`).limit(limit).offset(offset)
-      if (users.length > 0) {
-        await Promise.all(users.map(async user => {
-          const userId = user.id
-          let friendshipStatus = null
-          const friendship = await fastify.readDb('friends').where({ user_id: currentUserId, friend_id: userId }).orWhere({ user_id: userId, friend_id: currentUserId }).first()
-          if (friendship) {
-            friendshipStatus = 'friends'
-          } else {
-            const sentFriendRequest = await fastify.readDb('friend_requests').where({ requester_user_id: currentUserId, requested_user_id: userId }).first()
-            if (sentFriendRequest) {
-              friendshipStatus = 'pending_sent'
-            } else {
-              const receivedFriendRequest = await fastify.readDb('friend_requests').where({ requester_user_id: userId, requested_user_id: currentUserId }).first()
-              if (receivedFriendRequest) {
-                friendshipStatus = 'pending_received'
-              } else {
-                friendshipStatus = 'none'
-              }
-            }
-          }
-          user.friendship_status = friendshipStatus
-        }))
-        res.status(200).send(users)
-      } else {
-        res.status(404).send({ message: 'Not found' })
-      }
+      // const users = await fastify.readDb('users').whereILike('username', `%${query}%`).limit(limit).offset(offset)
+      // if (users.length > 0) {
+      //   await Promise.all(users.map(async user => {
+      //     const userId = user.id
+      //     let friendshipStatus = null
+      //     const friendship = await fastify.readDb('friends').where({ user_id: currentUserId, friend_id: userId }).orWhere({ user_id: userId, friend_id: currentUserId }).first()
+      //     if (friendship) {
+      //       friendshipStatus = 'friends'
+      //     } else {
+      //       const sentFriendRequest = await fastify.readDb('friend_requests').where({ requester_user_id: currentUserId, requested_user_id: userId }).first()
+      //       if (sentFriendRequest) {
+      //         friendshipStatus = 'pending_sent'
+      //       } else {
+      //         const receivedFriendRequest = await fastify.readDb('friend_requests').where({ requester_user_id: userId, requested_user_id: currentUserId }).first()
+      //         if (receivedFriendRequest) {
+      //           friendshipStatus = 'pending_received'
+      //         } else {
+      //           friendshipStatus = 'none'
+      //         }
+      //       }
+      //     }
+      //     user.friendship_status = friendshipStatus
+      //   }))
+      //   res.status(200).send(users)
+      // } else {
+      //   res.status(404).send({ message: 'Not found' })
+      // }
+      res.status(404).send({error: true, message: "Not found"})
     } catch (error) {
       console.log(error)
       res.status(500).send({ error })
