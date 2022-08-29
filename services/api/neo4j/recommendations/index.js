2f4eaacf1b17c69c2606bdd0a45f038c536f590c
Great Okonkwo <great@wishroll.co>
2022-08-29 01:56:08 -0400

    Refactor suggestions api

--- services/api/neo4j/recommendations/index.js
+++ services/api/neo4j/recommendations/index.js
@@ -8,16 +8,20 @@ async function getMutualFriends(userId, limit = 10, offset = 0) {
             WHERE NOT (user1)-[:FRIENDS_WITH]-(friend_of_friend)
             AND NOT (user1)-[:FRIEND_REQUESTED]-(friend_of_friend)
             AND user1.id <> friend_of_friend.id
-            RETURN friend_of_friend, COUNT(*)
-            ORDER BY COUNT(*) DESC , friend_of_friend
+            RETURN friend_of_friend.id as id, friend_of_friend.uuid as uuid,
+            friend_of_friend.username as username, friend_of_friend.display_name as display_name,
+            friend_of_friend.avatar_url as avatar_url, COUNT(*)
+            ORDER BY COUNT(*) DESC
             SKIP ${offset}
             LIMIT ${limit}
         `
-        const writeResult = await session.writeTransaction(tx =>
+        const result = await session.writeTransaction(tx =>
             tx.run(query)
         )
-        console.log(writeResult.summary)
-        return writeResult.records;
+        const records = result.records.map(r => {
+            return { id: r.get('id'), uuid: r.get('uuid'), username: r.get('username'), display_name: r.get('display_name'), avatar_url: r.get('avatar_url') }
+        });
+        return records;
     } catch (error) {
         console.log(error);
         return error
