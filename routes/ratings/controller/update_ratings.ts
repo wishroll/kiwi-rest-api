3074af9b0f46f231d90fa023378446744273e3f6
Krzysztof Goluszka <krzysztofgoluszka@skrzynias-MacBook-Pro.local>
2022-11-30 11:33:09 +0100

    [KIWI-27] Added endpoint for likes, and compatibility for old endpoint

---
+++ routes/ratings/controller/update_ratings.ts
@@ -0,0 +1,73 @@
+import { mapLikeToScore } from '../../../algos/users/score_likes_mappers';
+import logger from '../../../logger';
+import { WishrollFastifyInstance } from '../../index';
+
+export interface UpdateRatingAttributes {
+  currentUserId: number;
+  messageId: number;
+  like: boolean;
+}
+
+export default (fastify: WishrollFastifyInstance) =>
+  async ({ currentUserId, messageId, like }: UpdateRatingAttributes) =>
+    fastify.writeDb.transaction(async trx => {
+      const mappedScore = mapLikeToScore(like);
+
+      const messageScore = await trx('ratings')
+        .insert(
+          {
+            user_id: currentUserId,
+            message_id: messageId,
+            score: mappedScore,
+            like: like,
+          },
+          '*',
+        )
+        .then(rows => rows[0]);
+
+      logger(fastify).trace({ messageScore, currentUserId }, 'updated ratings table');
+
+      if (messageScore === null || typeof messageScore !== 'object') {
+        throw new Error('could not update message score/like');
+      }
+
+      const ratedMessage = await trx('messages')
+        .select('sender_id')
+        .where({ id: messageId })
+        .first();
+
+      logger(fastify).trace({ ratedMessage }, 'message that will be rated');
+
+      if (ratedMessage === null || typeof ratedMessage !== 'object') {
+        throw new Error('could not find message to score/like');
+      }
+
+      return trx('user_ratings')
+        .insert(
+          {
+            user_id: ratedMessage.sender_id,
+            score: mappedScore,
+            num_ratings: 1,
+            likes: like ? 1 : 0,
+          },
+          ['*'],
+        )
+        .onConflict('user_id')
+        .merge({
+          num_ratings: trx.raw(':currentCount: + 1', {
+            currentCount: 'user_ratings.num_ratings',
+          }),
+          score: trx.raw(
+            '((:currentScore: * :currentCount:) + :newScore ) / (:currentCount: + 1)',
+            {
+              currentScore: 'user_ratings.score',
+              currentCount: 'user_ratings.num_ratings',
+              newScore: mappedScore,
+            },
+          ),
+          likes: trx.raw(':currentLikes: + :newLike', {
+            currentLikes: 'user_ratings.likes',
+            newLike: like ? 1 : 0,
+          }),
+        });
+    });
---
+++ routes/ratings/index.ts
@@ -0,0 +1,68 @@
+import { updateMessageSenderRating } from '../../algos/users/update_message_sender_rating';
+import { sendNotificationOnCreatedRating } from '../../services/notifications/notifications';
+import logger from '../../logger';
+import { mapScoreToLike } from '../../algos/users/score_likes_mappers';
+import create from './schema/v1/create';
+import { WishrollFastifyInstance } from '../index';
+import { withErrorHandler } from '../../utils/errors';
+import { RateSongBody, RateSongParams, rateSongSchema } from './schema/v2/create';
+import { default as updateRatingController } from './controller/update_ratings';
+
+export default async (fastify: WishrollFastifyInstance) => {
+  fastify.post(
+    '/v1/messages/:message_id/ratings',
+    { onRequest: [fastify.authenticate], schema: create },
+    async (req, res) => {
+      // @ts-ignore
+      const messageId = req.params.message_id;
+      // @ts-ignore
+      const currentUserId = req.user.id;
+      // @ts-ignore
+      const score = req.body.score;
+      try {
+        const inserts = await fastify
+          .writeDb('ratings')
+          .insert(
+            { user_id: currentUserId, message_id: messageId, score, like: mapScoreToLike(score) },
+            ['*'],
+          );
+        if (inserts.length > 0) {
+          updateMessageSenderRating(messageId, score);
+          sendNotificationOnCreatedRating(messageId).catch(error => logger(req).error(error));
+          res.status(201).send();
+        } else {
+          res.status(400).send({ error: true, message: 'Failed to create rating' });
+        }
+      } catch (error) {
+        res.status(500).send({ error: true, message: error });
+      }
+    },
+  );
+
+  fastify.get(
+    '/v1/messages/:message_id/rating',
+    { onRequest: fastify.authenticate },
+    async (_req, _res) => {
+      // Send Song Message ID
+    },
+  );
+
+  fastify.post<{ Params: RateSongParams; Body: RateSongBody }>(
+    '/v2/messages/:message_id/ratings',
+    { onRequest: [fastify.authenticate], schema: rateSongSchema },
+    withErrorHandler(async (req, res) => {
+      // @ts-ignore
+      const currentUserId = req.user.id;
+      const like = req.body.like;
+      const messageId = req.params.message_id;
+
+      await updateRatingController(fastify)({
+        currentUserId,
+        like,
+        messageId,
+      });
+
+      res.status(201).send();
+    }),
+  );
+};
