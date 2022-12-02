84fb2fef3c931cd8c0a8fdf3056bbdce1af7de20
Krzysztof Goluszka <40217932+goluszka@users.noreply.github.com>
2022-12-02 18:38:41 +0100

    [KIWI-27] Added endpoint for likes, and compatibility for old endpoint (#217)
    * [KIWI-27] Added endpoint for likes, and compatibility for old endpoint
    
    * Added fix due to code review
    
    * [KIWI-29] Added endpoint to fetch not rated songs (#218)
    
    Co-authored-by: Krzysztof Goluszka <krzysztofgoluszka@skrzynias-MacBook-Pro.local>
    
    Co-authored-by: Krzysztof Goluszka <krzysztofgoluszka@skrzynias-MacBook-Pro.local>

--- routes/messages/schema/v2/index.ts
+++ routes/messages/schema/v2/index.ts
@@ -1,3 +1,4 @@
+import { FromSchema } from 'json-schema-to-ts';
 import { STREAMING_PLATFORMS } from '../../../../utils/const';
 
 export const receivedMessagesIndex = {
@@ -217,3 +218,118 @@ export const sentTracksIndex = {
     },
   },
 };
+
+export const receivedNewMessagesQuery = {
+  type: 'object',
+  properties: {
+    limit: { type: 'integer', minimum: 1 },
+    lastId: { type: 'integer', description: 'Id of last message in previous iteration' },
+  },
+  required: ['limit'],
+} as const;
+
+export const receivedNewMessagesIndex = {
+  description: 'Return a list of song messages that have not been rated yet.',
+  tags: ['Messages', 'new'],
+  summary: "Returns an array of a user's receieved song messages without score",
+  query: receivedNewMessagesQuery,
+  headers: {
+    type: 'object',
+    properties: {
+      Authorization: { type: 'string', description: 'The token used for authentication' },
+    },
+    required: ['Authorization'],
+  },
+  response: {
+    200: {
+      description: 'The request was successful',
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          id: { type: 'integer', minimum: 1, description: 'The primary key id of the message' },
+          uuid: { type: 'string', description: 'The unique alphanumeric id of the message' },
+          created_at: { type: 'string', description: 'The datetime when the message was created' },
+          updated_at: {
+            type: 'string',
+            description: 'The last timestamp when the message was updated',
+          },
+          text: { type: 'string' },
+          last_sender_id: { type: 'integer' },
+          seen: { type: 'boolean' },
+          sender: {
+            type: 'object',
+            properties: {
+              id: {
+                type: 'integer',
+                minimum: 1,
+                description: 'The id of the user who created the message',
+              },
+              uuid: { type: 'string' },
+              display_name: { type: 'string' },
+              username: { type: 'string' },
+              avatar_url: { type: 'string' },
+            },
+          },
+          track: {
+            type: 'object',
+            properties: {
+              track_id: { type: 'string' },
+              platform: { type: 'string', enum: STREAMING_PLATFORMS },
+              uri: { type: 'string' },
+              external_url: { type: 'string' },
+              href: { type: 'string' },
+              name: { type: 'string' },
+              duration: { type: 'integer' },
+              track_number: { type: 'integer' },
+              release_date: { type: 'string' },
+              isrc: { type: 'string' },
+              artists: {
+                type: 'array',
+                items: {
+                  type: 'object',
+                  properties: {
+                    id: { type: 'string' },
+                    name: { type: 'string' },
+                    uri: { type: 'string' },
+                    href: { type: 'string' },
+                  },
+                },
+                required: ['id', 'name', 'href'],
+              },
+              explicit: { type: ['boolean'] },
+              artwork: {
+                type: 'object',
+                properties: {
+                  width: { type: 'integer' },
+                  height: { type: 'integer' },
+                  url: { type: 'string' },
+                },
+                required: ['url'],
+              },
+            },
+          },
+        },
+        required: ['id', 'uuid', 'created_at', 'updated_at', 'text'],
+      },
+    },
+    404: {
+      description: 'Not found',
+      type: 'object',
+      properties: {
+        error: { type: 'boolean' },
+        message: { type: 'string' },
+      },
+    },
+    500: {
+      description: 'Internal Server Error',
+      type: 'object',
+      properties: {
+        error: { type: 'boolean' },
+        message: { type: 'string' },
+      },
+    },
+  },
+};
+
+export type NewSongsQuery = FromSchema<typeof receivedNewMessagesQuery>;
