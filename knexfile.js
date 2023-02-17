cc0bdf006d7e1f749a1b9be1f827a0b20d74df30
tboba <tymoteusz.boba@gmail.com>
2023-02-17 16:14:22 +0100

    Protect staging and production envs from running seeds

--- knexfile.js
+++ knexfile.js
@@ -38,6 +38,7 @@ module.exports = {
     migrations: {
       directory: './migrations',
     },
+    seeds: { directory: 'please-do-not-use-seeds-on-staging' },
   },
 
   production: {
@@ -53,7 +54,7 @@ module.exports = {
     migrations: {
       directory: './services/db/postgres/migrations',
     },
-    seeds: { directory: './services/db/postgres/seeds' },
+    seeds: { directory: 'please-do-not-use-seeds-on-production' },
     useNullAsDefault: true,
   },
 };
