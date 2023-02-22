c69bb83c48c31b783580e800954b95ad9b80e2fb
priteshkadiwala <pk.45@hotmail.com>
2023-02-22 19:44:44 +0530

    fix: removed prehandler

--- routes/users/index.js
+++ routes/users/index.js
@@ -308,27 +308,23 @@ module.exports = async (fastify, _options) => {
     },
   );
 
-  fastify.put(
-    '/users/playlistId',
-    { onRequest: [fastify.authenticate], preHandler: upload.single('avatar') },
-    async (req, res) => {
-      const userId = req.user.id;
-      const updateParams = req.body;
+  fastify.put('/users/playlistId', { onRequest: [fastify.authenticate] }, async (req, res) => {
+    const userId = req.user.id;
+    const updateParams = req.body;
 
-      try {
-        const results = await fastify
-          .writeDb('users')
-          .select('id')
-          .where({ id: userId })
-          .update(updateParams, ['playlist_id']);
+    try {
+      const results = await fastify
+        .writeDb('users')
+        .select('id')
+        .where({ id: userId })
+        .update(updateParams, ['playlist_id']);
 
-        res.status(200).send(results[0]);
-      } catch (error) {
-        logger(req).error(error);
-        res.status(500).send({ error: true, message: 'An error occured' });
-      }
-    },
-  );
+      res.status(200).send(results[0]);
+    } catch (error) {
+      logger(req).error(error);
+      res.status(500).send({ error: true, message: 'An error occured' });
+    }
+  });
 
   /**
    * Version 1
