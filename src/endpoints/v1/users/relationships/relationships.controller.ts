272330b8bb4a062a5f47bad1b8b45fadfeacb251
Great Okonkwo <great@wishroll.co>
2023-09-26 14:27:07 -0400

    fix: fix ts errors

--- src/endpoints/v1/users/relationships/relationships.controller.ts
+++ src/endpoints/v1/users/relationships/relationships.controller.ts
@@ -1,12 +1,12 @@
 import { FastifyInstance, FastifyPluginAsync } from "fastify";
-import { createRelationshipHandler, deleteRelationshipHandler, getRelationshipsHandler, createRelationshipRequestHandler, deleteRelationshipRequest, getRequestedRelationshipsHandler, getRequestingRelationshipsHandler, getRelationshipStatusHandler, getRelationshipSuggestions } from "./relationships.handler";
+import { createRelationshipHandler, deleteRelationshipHandler, getRelationshipsHandler, createRelationshipRequestHandler, deleteRelationshipRequestHandler, getRequestedRelationshipsHandler, getRequestingRelationshipsHandler, getRelationshipStatusHandler, getRelationshipSuggestions } from "./relationships.handler";
 
 const relationships: FastifyPluginAsync = async (fastify: FastifyInstance): Promise<void> => {
     fastify.post('/accept-request', createRelationshipHandler);
     fastify.delete('/', deleteRelationshipHandler);
     fastify.get('/', getRelationshipsHandler);
     fastify.post('/request', createRelationshipRequestHandler);
-    fastify.delete('/request', deleteRelationshipRequest);
+    fastify.delete('/request', deleteRelationshipRequestHandler);
     fastify.get('/requested', getRequestedRelationshipsHandler);
     fastify.get('/requesting', getRequestingRelationshipsHandler);
     fastify.get('/status', getRelationshipStatusHandler);
